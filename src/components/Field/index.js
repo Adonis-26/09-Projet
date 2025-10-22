f96fc18e56871a9670675d4767975e09e78148e8
Adonis YAMB NDEBY <ayambndeby@hotmail.fr>
2025-10-22 16:56:19 +0200

    je retire le required le composant TEXTAREA

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -36,8 +36,7 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
     case FIELD_TYPES.TEXTAREA:
       component = <textarea name={name} 
         data-testid="field-testid" 
-        required
-        minLength={10}/>;
+        />;
       break;
     default:
       component = (
